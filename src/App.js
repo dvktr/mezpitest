f8e17e060acbbe97355e3711475a906af4bd3494
dvktr <victormendes04012004@gmail.com>
2020-07-13 15:38:36 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <d1>eae</d1>
+    </div>
+  );
+}
+
+export default App;
